fb41430baf9b31c52ad60c66ff7893e80d392976
jchua1 <jchua@stuy.edu>
2016-11-04 15:35:28 -0400

    worked on story creation

--- utils/storyCreate.py
+++ utils/storyCreate.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-def storyExists(story, c):
+def storyExists(title, c):
     s = c.execute('SELECT name FROM stories')
     for r in s:
         name = r[0]
@@ -8,5 +8,18 @@ def storyExists(story, c):
             return True;
     return False;
 
-def addStory():
-    
+def addStory(title, user, text):
+    result = []
+    bd = sqlite3.connect('data/bd.db')
+    c = bd.cursor()
+    if (storyExists(title, c)):
+        result = ['Story already exists.', False]
+    elif len(title) == 0 or len(text) == 0:
+         result = ['Missing title/text.', False]
+    else:
+        c.execute('CREATE TABLE %s (name TEXT, content TEXT)'%(title))
+        c.execute('INSERT INTO %s VALUES ("%s", "%s")'%(title, user, text))
+        bd.commit()
+        bd.close()
+        result = ['Creation successful.', False]
+    return result
