6326a318993b71ef4ec6cb98c7996862948133b4
jchua1 <jchua@stuy.edu>
2016-11-06 18:07:42 -0800

    redirects to login page if accessing a route while not logged in

--- app.py
+++ app.py
@@ -11,20 +11,26 @@ def login():
         return redirect(url_for('home'))
     return render_template('login.html')
 
-@app.route('/home/', methods = ['POST', 'GET'])
+@app.route('/home/')
 def home():
-    my = homeDisp.myStoryList(session['user'])
-    non = homeDisp.nonStoryList(session['user'])
-    myMsg = nonMsg = ""
-    if (len(my) == 0):
-        myMsg = "You haven't contributed to any stories :("
-    if (len(non) == 0):
-        nonMsg = "You've contributed to all the stories :)"
-    return render_template('home.html', user = session['user'], myStories = my, nonStories = non, myMsg = myMsg, nonMsg = nonMsg)
+    if 'user' in session:
+        my = homeDisp.myStoryList(session['user'])
+        non = homeDisp.nonStoryList(session['user'])
+        myMsg = nonMsg = ""
+        if (len(my) == 0):
+            myMsg = "You haven't contributed to any stories :("
+        if (len(non) == 0):
+            nonMsg = "You've contributed to all the stories :)"
+        return render_template('home.html', user = session['user'], myStories = my, nonStories = non, myMsg = myMsg, nonMsg = nonMsg)
+    else:
+        return redirect(url_for('login'))
 
 @app.route('/write/')
 def write():
-    return render_template('write.html')
+    if 'user' in session:
+        return render_template('write.html')
+    else:
+        return redirect(url_for('login'))
 
 @app.route('/authenticate/', methods = ['POST'])
 def authenticate():
@@ -49,7 +55,7 @@ def create():
     else:
         return render_template('write.html', messageCreate = data[0])
 
-@app.route('/renderStory/', methods = ['POST','GET'])
+@app.route('/renderStory/', methods = ['POST'])
 def renderStory():
     storyTitle = request.form['title']
     storyAuthor = storyDisp.genAuthor(storyTitle)
@@ -68,13 +74,12 @@ def addComment():
     storyCreate.addComment(user, storyTitle, storyText)
     storyDict = storyDisp.genStory(storyTitle)
     storyAuthor = storyDisp.genAuthor(storyTitle)
-    #return render_template('view.html', story = storyDict, title = storyTitle, author = storyAuthor)
     return redirect(url_for('renderStory'), code = 307)
-
 
 @app.route('/logout/')
 def logout():
-    session.pop('user')
+    if 'user' in session:
+        session.pop('user')
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
